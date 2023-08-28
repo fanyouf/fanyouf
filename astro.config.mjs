1337aaf09f4cc1b5b4fb1f4f1d4622c50a97704e
Your Name <you@example.com>
2023-08-28 16:13:10 +0800

    add

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://fanyouf.github.io',
-	base: '/',
+	// base: './',
 	integrations: [mdx(), sitemap()],
 });
